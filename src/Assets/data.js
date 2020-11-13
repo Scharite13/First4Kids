10cbcb2c34f74bcb8c6e6e227d72599c4a5ee155
Derek Scharite <dms121691@gmail.com>
2020-11-13 14:53:05 -0500

    about page build out

---
+++ src/Assets/data.js
@@ -0,0 +1,26 @@
+export const brochure = [
+    {
+        intro1: `We are a faith based childcare center that offers innovative, year
+                round, full day educational pre-school as well as infant care.  Our mission is
+                to provide a safe, loving, and encouraging environment that meets children's developmental and academic
+                needs as well as their spiritual ones.  It is our mission to instill confidence, 
+                self-worth, and Christian morals in every child.`,
+        intro2: `We're here to provide you with peace of mind from the moment you drop 
+                off your child to the moment you pick them up.  All staff members are required 
+                by the state to have taken 45 hours in childcare training.  In addition to this training, an additional
+                10 hours of training must be done each year.  Staff must also continue to renew 
+                theri CPR and First Aid certifications.  All staff are cleared through DCF, the Federal
+                Bureau of investigation, and the Florida Department of Law Enforcement.  We are also 
+                required to screen staff on the Sex Offender registry and the Child Abuse and 
+                Neglect registry for any state they have resided for the past 5 years.`,
+        intro3: `Safety is an important part of what we offer at First for Kids.  Each family is given the access 
+                code in order to enter the school.  This locked entrance serves as a safety guard for us as well
+                as for you and your children.  Please note that we do have video cameras in each classroom, in the hallways, 
+                and outside.  Ther director's office has access to them for additional security.  Parents
+                are invited to come by anytime to visit.`
+             
+    }
+]
+
+
+export default brochure;
--- src/Pages/Aboutpage/aboutpage.js
+++ src/Pages/Aboutpage/aboutpage.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import './aboutpage.styles.css';
+import { brochure } from '../../Assets/data';
+import biblePic from '../../Images/christian_preschool.png';
+import happyKids from '../../Images/happy_kids.jpg'
+
+const intro = brochure[0]
+
 
 const AboutPage = () => (
+     
     <div className="about-page">
-        <h1>About Page</h1>
+        <h1 className="title">Our School</h1>
+        <div className="intro-container">
+            <p className="text">{intro.intro1}</p> 
+            <img src={biblePic} height='250px' width='406px'/>  
+        </div>
+        <div className="intro-container">
+            <img src={happyKids} height='250px' width='406px'/>
+            <p className="text">{intro.intro2}</p>
+        </div>
+        <div className="intro-container">
+            <p className="text">{intro.intro3}</p>
+            <div className="lock">
+            <i className="fas fa-lock"></i>
+            </div>
+        </div>
+        
+        
     </div>
 )
 
