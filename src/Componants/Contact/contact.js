3579fbd849d9d40db3ba34c092847c3828bfea28
Derek Scharite <dms121691@gmail.com>
2020-12-05 14:37:33 -0500

    add state to contact form

--- src/Componants/Contact/contact.js
+++ src/Componants/Contact/contact.js
@@ -1,20 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import axios from 'axios';
 import './contact.styles.css';
 
-const Contact = () => (
+class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
+        };
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+        this.handleMessageChange = this.handleMessageChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    
+
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.state.name, this.state.email, this.state.phone, this.state.message)
+        this.resetForm()
+    };
+
+    resetForm(){
+        this.setState({name: '', email: '', phone: '', message: ''})
+    }
+
+  
+
+    handleNameChange(event){
+        this.setState({name: event.target.value})
+    }
+    handleEmailChange(event){
+        this.setState({email: event.target.value})
+    }
+    handlePhoneChange(event){
+        this.setState({phone: event.target.value})
+    }
+    handleMessageChange(event){
+        this.setState({message: event.target.value})
+    }
+    
+
+    render(){
+        return (
     <div>
-        <form className="contact-form" id="contact-form" method="post" action="contact.php">
-            <label for="name">Name:</label><br></br>
-            <input type="text" id="name" name="name" placeholder="Name"/><br></br>
-            <label for="email">E-mail:</label><br></br>
-            <input type="email" id="email" name="email" placeholder="JohnDoe@email.com"/><br></br>
-            <label for="phone">Phone:</label><br></br>
-            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/><br></br>
-            <label for="message">Message:</label><br></br>
-            <textarea name="message" rows="4" cols="35" id="comment"></textarea><br></br>
-            <button type="submit" action="blank email">Submit</button>
+        <form className="contact-form" id="contact-form" onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Name:</label><br></br>
+            <input type="text" id="name" name="name" onChange={this.handleNameChange}  value={this.state.name} placeholder="Name"/><br></br>
+            <label htmlFor="email">E-mail:</label><br></br>
+            <input type="email" id="email" name="email" onChange={this.handleEmailChange} value={this.state.email}  placeholder="JohnDoe@email.com"/><br></br>
+            <label htmlFor="phone">Phone:</label><br></br>
+            <input type="tel" id="phone" onChange={this.handlePhoneChange} value={this.state.phone} name="phone"/><br></br>
+            <label htmlFor="message">Message:</label><br></br>
+            <textarea name="message" rows="4" cols="35" id="comment" onChange={this.handleMessageChange} value={this.state.message}></textarea><br></br>
+            <button type="submit" value='submit'>Submit</button>
         </form>
     </div>
-)
+        )}}
 
 export default Contact;
--- src/Componants/Footer/Footer.js
+++ src/Componants/Footer/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { faq } from '../../Assets/data';
 import "./Footer.styles.css";
 
 function Footer() {
@@ -7,14 +6,14 @@ function Footer() {
         <div className="footer-container">
             <div className="map-contact-container">
                 <div className="map">
-                    <h3 className="f-title"><i class="fas fa-map-marker-alt"></i>Find Us</h3>
+                    <h3 className="f-title"><i className="fas fa-map-marker-alt"></i>Find Us</h3>
 
                     <h4>First United Methodist Church</h4>
                     <p>603 11th St. W. | Bradenton FL 34205</p>
 
                 </div>
                 <div className="contact">
-                    <h3 className="f-title"><i class="fas fa-phone-volume"></i>Contact Us</h3>
+                    <h3 className="f-title"><i className="fas fa-phone-volume"></i>Contact Us</h3>
                     <a href="mailto: learn@firstforkids.net">learn@firstforkids.net</a>
                     <p>941-748-0824</p>
                 </div>
